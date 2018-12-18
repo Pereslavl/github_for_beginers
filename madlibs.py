22bec00ff0d3efe8d5b6bda89bd3381e60fc8acd
Sergey Kryukov <cydkom@gmail.com>
2018-12-18 22:15:09 +0300

    version 2.0 branch

--- madlibs.py
+++ madlibs.py
@@ -5,17 +5,8 @@ var_adjective = input("Please enter a adjective ")
 
 # Template:
 #     I enjoy practice! I find it help me to __(verb)__better
-print("I enjoy practice! I find it help me to {} better".format(var_verb))
+print("I enjoy practice! I find it help me to", var_verb, "better.")
 #     Without practice, my __(none)___ would probably not even work.
-print("Without practice, my {} would probably not even work.".format(var_noun))
+print("Without practice, my", var_noun  "would probably not even work.")
 #     My code is getting more __(adjective)__ every single day!
-print("My code is getting more {} every single day!".format(var_adjective))
-
-
-# TODO: Prompt the user for parts of speach and store  them in the  variables
-
-
-
-
-
-# TODO:  Output the temple to the screen the blanks filled out with what the user stated
+print("My code is getting more", var_adjective,  "every single day!")
