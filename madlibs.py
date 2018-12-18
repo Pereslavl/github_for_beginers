1e6fb75c7434280f5dee49a2749ca318613dc42e
Sergey Kryukov <cydkom@gmail.com>
2018-12-18 22:04:59 +0300

    version 1.0
    we've edited the error

--- madlibs.py
+++ madlibs.py
@@ -1,13 +1,15 @@
 # Madlibs - Practice Input and Output
-var_verb = input(Please enter a verb)
-var_noun = input(Please enter a none)
-var_adjective = input(Please enter a adjective)
+var_verb = input("Please enter a verb ")
+var_noun = input("Please enter a none ")
+var_adjective = input("Please enter a adjective ")
 
 # Template:
 #     I enjoy practice! I find it help me to __(verb)__better
+print("I enjoy practice! I find it help me to {} better".format(var_verb))
 #     Without practice, my __(none)___ would probably not even work.
+print("Without practice, my {} would probably not even work.".format(var_noun))
 #     My code is getting more __(adjective)__ every single day!
-
+print("My code is getting more {} every single day!".format(var_adjective))
 
 
 # TODO: Prompt the user for parts of speach and store  them in the  variables
